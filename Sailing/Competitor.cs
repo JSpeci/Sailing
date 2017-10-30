a6f16162ad5ecf6c7bfbc6ee70ab8726b711b810
Jan_Specian <jan.specian@tul.cz>
2017-10-30 17:44:25 +0100

    competitor and race class

---
+++ Sailing/Competitor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/** Class represents one competitor in race
+ *  
+ **/ 
+namespace Sailing
+{
+    class Competitor
+    {
+        private String nameOfTeam;
+
+        public String NameOfTeam
+        {
+            get { return this.nameOfTeam; }
+            set { this.nameOfTeam = value; }
+        }
+
+        public Competitor(String name)
+        {
+            this.nameOfTeam = name;
+        }
+
+        public override string ToString()
+        {
+            return "Competitor: " + nameOfTeam;
+        }
+
+    }
+}
---
+++ Sailing/Race.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sailing
+{
+    class Race
+    {
+        
+        public struct CompetitorResult
+        {
+            public Competitor competitor;
+            public int order;
+        }
+
+        private int numberOfCompetitors;
+        private List<CompetitorResult> raceResult;
+        
+
+        public int NumberOfTeams
+        {
+            get { return this.numberOfCompetitors; }
+        }
+
+        public List<CompetitorResult> RaceResult
+        {
+            get { return this.raceResult; }
+        }
+
+        public Race(String pathToCsv)
+        {
+            this.raceResult = new List<CompetitorResult>();
+            loadDataFromCsv(pathToCsv);
+            this.numberOfCompetitors = this.raceResult.Count;
+        }
+            
+        private void loadDataFromCsv(String path)
+        {
+            String[] allLines = System.IO.File.ReadAllLines(path);
+            char[] separator = new char[1];
+            separator[0] = ',';
+
+            for (int x = 0; x < allLines.Length; x++)
+            {
+                String[] row = allLines[x].Split(separator, 2);
+                Console.WriteLine(row[0] + " " + row[1]);
+            }
+        }
+
+    }
+}
